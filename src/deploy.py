ab9720e2106a44fc90fa40fdbcec4c8febe684c4
Adam Grassart <AdamGrassart@users.noreply.github.com>
2019-08-30 22:05:01 +0200

    Construction du manager par appel de methode dynamique

--- src/deploy.py
+++ src/deploy.py
@@ -1,14 +1,28 @@
-import argparse
+import argparse, inspect
 
 class Deploy:
 
     def __init__(self, args):
-        pass
+
+        self.command = args.command
         # args.[command] <- vérification existence de méthode
         # args.[command} <- appel dynamique de methode
 
 
+    def run_manager(self):
+        command = 'action_' + self.command
+        is_hasattr  = hasattr(self, command)
 
+        if is_hasattr and inspect.ismethod(getattr(self, command)):
+            getattr(self, command)()
+
+
+
+    def action_on(self):
+        print("appel de la methode on")
+
+    def action_remote(self):
+        print("appel de la methode remote")
 
 
 if __name__ == '__main__':
@@ -31,4 +45,6 @@ if __name__ == '__main__':
     )
 
     args = parser.parse_args()
+
     deploy = Deploy(args)
+    deploy.run_manager()
