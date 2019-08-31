4f01d8cc8672f6c64565a5cebc2b867793b9ef19
Adam Grassart <AdamGrassart@users.noreply.github.com>
2019-08-31 14:04:33 +0200

    Developpement du parser yaml + gestion des erreurs

--- src/confparser.py
+++ src/confparser.py
@@ -2,21 +2,32 @@ import yaml
 
 class ConfParser():
 
-    def __init__(self, remote):
-        self.remote = remote
+    def __init__(self, server_name):
+        self.server_name = server_name
         self.conf = self._parseFile()
+
+        print(self.conf)
 
         # format conf :
         #self.conf["website1"]["ssh"]
 
 
     def _parseFile(self):
-        # todo : handle error for failed open file
-        with open("deploy.yaml", 'r') as stream:
-            try:
-                return yaml.safe_load(stream)
-            except yaml.YAMLError as exc:
-                print(exc)
+        try:
+            with open("deploy.yaml", 'r') as stream:
+                try:
+                    return yaml.safe_load(stream)[self.server_name]
+                except yaml.YAMLError as exc:
+                    self._err("Le fichier de configuration n'est pas bien formaté")
+                except KeyError:
+                    self._err("Ce nom de serveur n'existe pas dans le fichier de configuration")
+        except IOError:
+            self._err("Chargement impossible du fichier deploy.yaml")
+            
+
+    def _err(self, msg):
+        print(msg)
+        exit(1)
 
 
 
