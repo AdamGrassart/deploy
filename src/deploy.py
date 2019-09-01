56ed143796d7a48202b982dbff09b0454eefc1b7
Adam Grassart <AdamGrassart@users.noreply.github.com>
2019-09-01 11:13:28 +0200

    Modif logique de chargement ssh et conf

--- src/deploy.py
+++ src/deploy.py
@@ -9,6 +9,14 @@ class Deploy:
     def __init__(self, args):
         self.command = args.command
         self.server_name = args.server_name
+        self.git = Git()
+
+        if self.server_name != '':
+            self.conf = self._load_conf()
+            self.ssh  = Ssh(self.conf.ssh_user_host,
+                            self.conf.ssh_port,
+                            self.conf.server_dest)
+
 
     def run_manager(self):
         command = 'action_' + self.command
@@ -17,16 +25,16 @@ class Deploy:
         if is_hasattr and inspect.ismethod(getattr(self, command)):
             getattr(self, command)()
 
+    def _load_conf(self):
+        return ConfParser(self.server_name)
+
     def action_on(self):
         # appel de la methode on pour envoyer sur le serveur
         pass
 
     def action_check(self):
-        conf = ConfParser(self.server_name)
-        ssh = Ssh(conf.ssh_user_host, conf.ssh_port, conf.server_dest)
-        branch, sha = ssh.get_rev()
-        git = Git()
-        git.branch
+        branch, sha = self.ssh.get_rev()
+        self.git.branch
 
         # 3.bis -> on vérifie que l'on est sur la même branche en local
         # 4. -> on compare sha | HEAD
