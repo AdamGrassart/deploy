4380836351796d83894cd4644f93bd24e386abab
Adam Grassart <AdamGrassart@users.noreply.github.com>
2019-09-03 15:17:37 +0200

    utilisation de la classe Error dans le parseur de configuration

--- src/confparser.py
+++ src/confparser.py
@@ -1,26 +1,24 @@
 import yaml
+from error import Error
 
 class ConfParser():
 
     def __init__(self, server_name):
+        self.error = Error()
         self.server_name = server_name
         self.conf = self._parseFile()
 
-        # format conf :
-        #self.conf["website1"]["ssh"]
-
-
     def _parseFile(self):
         try:
-            with open("deploy.yaml", 'r') as stream:
+            with open("deploy.yamla", 'r') as stream:
                 try:
                     return yaml.safe_load(stream)[self.server_name]
                 except yaml.YAMLError as exc:
-                    self._err("Le fichier de configuration n'est pas bien formaté")
+                    self.error.stop_with_msg("Le fichier de configuration n'est pas bien formaté")
                 except KeyError:
-                    self._err("Ce nom de serveur n'existe pas dans le fichier de configuration")
+                    self.error.stop_with_msg("Ce nom de serveur n'existe pas dans le fichier de configuration")
         except IOError:
-            self._err("Chargement impossible du fichier deploy.yaml")
+            self.error.stop_with_msg("Chargement impossible du fichier deploy.yaml")
 
     @property
     def ssh_user_host(self):
@@ -44,12 +42,5 @@ class ConfParser():
             'port': self.ssh_port,
             'dest': self.ssh_dest
         }
-
-
-
-    def _err(self, msg):
-        print(msg)
-        exit(1)
-
 
 
